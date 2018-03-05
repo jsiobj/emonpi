74e8d9d0febac97cadbf228619528f22e96d7ffc
jsiobj <js@interactingobjects.com>
2018-03-05 21:23:46 +0000

    Archiving logfiles every hour (cron.hourly) A bit of cleanup

--- logtools/varlog-backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo "======== Log file backup" >> /var/log/varlog-backup.log
-date >> /var/log/varlog-backup.log
-rsync -av /var/log/* /home/pi/data/logtools/log.backup >> /var/log/varlog-backup.log 2>&1
-echo "======== End" >> /var/log/varlog-backup.log
